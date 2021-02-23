3f5117d2cf4bd9e1943f5a417d0fc5a8fa1ad319
Jane Wu <conchwu@hotmail.com>
2021-02-23 17:00:14 +0800

    add: flowable

--- src/main/java/com/j/demo/flowablespringboot/FlowablespringbootApplication.java
+++ src/main/java/com/j/demo/flowablespringboot/FlowablespringbootApplication.java
@@ -1,10 +1,14 @@
 package com.j.demo.flowablespringboot;
 
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-
-
+//@SpringBootApplication(proxyBeanMethods = false)
 @SpringBootApplication
 public class FlowablespringbootApplication {
 
@@ -12,4 +16,22 @@ public class FlowablespringbootApplication {
         SpringApplication.run(FlowablespringbootApplication.class, args);
     }
 
+
+    @Bean
+    public CommandLineRunner init(final RepositoryService repositoryService,
+                                  final RuntimeService runtimeService,
+                                  final TaskService taskService) {
+
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... strings) throws Exception {
+                System.out.println("Number of process definitions : "
+                        + repositoryService.createProcessDefinitionQuery().count());
+                System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+                runtimeService.startProcessInstanceByKey("oneTaskProcess");
+                System.out.println("Number of tasks after process start: "
+                        + taskService.createTaskQuery().count());
+            }
+        };
+    }
 }
