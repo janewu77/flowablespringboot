ae63ec65297c27119c654fd14238efe6821590a6
Jane Wu <conchwu@hotmail.com>
2021-03-08 11:34:03 +0800

    update：demo代码整理

--- src/main/java/com/j/demo/flowablespringboot/flowable/demo/FlowableController.java
+++ src/main/java/com/j/demo/flowablespringboot/flowable/demo/FlowableController.java
@@ -5,10 +5,15 @@ import com.j.demo.flowablespringboot.flowable.FlowableService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.form.api.FormInfo;
+import org.flowable.task.api.history.HistoricTaskInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Api(tags = "Flowable 演示", description = "demo: flowable")
@@ -33,6 +38,36 @@ public class FlowableController {
     @GetMapping("/fetchTaskInfo")
     public void getATask(@RequestParam(value = "taskId") String taskId) {
         flowableService.fetchTaskFromHistory(taskId);
+    }
+
+    @GetMapping("/fetchAllTask")
+    public Object getAllTask() {
+        List<HistoricTaskInstance> tasks = flowableService.fecthTaskListFromHistory("DemoFormProcess");
+
+        List<String> result = new ArrayList<>();
+        for(HistoricTaskInstance task:tasks){
+            result.add( "["+task.getId() +"]:"+ task.getName());
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/showTaskForm")
+    public FormInfo doDemoShowTaskFrom(@RequestParam(value = "taskId") String taskId) {
+        //显示 task的表单 （带数据）
+        return flowableService.fetchForm4Task(taskId);
+    }
+
+
+    @PostMapping("/completeTask")
+    public void dodemoCompleteTask(@RequestParam(value = "taskId") String taskId) {
+        demoService.demoCompleteTask(taskId);
+    }
+
+    //
+    @PostMapping("/completeTaskWithForm")
+    public void doCompleteTaskWithFrom(@RequestParam(value = "taskId") String taskId,
+                                       @RequestParam(value = "no") String no) {
+            demoService.demoCompleteTaskWithForm(taskId);
     }
 
     @PostMapping("/startProcess")
--- src/main/java/com/j/demo/flowablespringboot/flowable/demo/FlowableDemoService.java
+++ src/main/java/com/j/demo/flowablespringboot/flowable/demo/FlowableDemoService.java
@@ -5,6 +5,9 @@ import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
 import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.form.api.FormRepositoryService;
+
+import org.flowable.engine.*;
 import org.flowable.task.api.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -30,6 +33,33 @@ public class FlowableDemoService {
     @Autowired
     RuntimeService runtimeService;
 
+    @Autowired
+    FormService formService;
+
+    @Autowired
+    FormRepositoryService formRepositoryService;
+
+
+
+    public void demoCompleteTask(String taskId){
+        taskService.complete(taskId);
+    }
+
+
+    public void demoCompleteTaskWithForm(String taskId){
+
+        Task task = flowableService.fetchTaskFromRuntime(taskId);
+
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("other",  task.getName()+".other.value");  //实际上没有保存
+        variables.put("operation", task.getName()+".operation.value");//required
+        variables.put("operation_remark", task.getName()+".operation_remark.value");
+        variables.put("remark", task.getName()+".remark.value");
+        variables.put("approve",true);
+
+        flowableService.doCompleteTask(taskId,variables);
+    }
+
 
     //演示：
     //演示变量在task、execution、process上的可见性
@@ -43,13 +73,7 @@ public class FlowableDemoService {
         ProcessInstance processInstance = flowableService.startProcess(pKey);
 
         //取得第一个task : <userTask id="taskform1" name ="usertask1" >
-        Optional<Task> optTask = taskService.createTaskQuery()
-                .processInstanceId(processInstance.getId())
-                .taskDefinitionKey("taskform1")
-                .includeProcessVariables()
-                .includeTaskLocalVariables()
-                .listPage(0,1)
-                .stream().findFirst();
+        Optional<Task> optTask = _getTask(processInstance.getId(),"taskform1");
 
         if(optTask.isPresent()){
             Task task1 = optTask.get();
@@ -61,6 +85,8 @@ public class FlowableDemoService {
             //设置变量
             task1.setAssignee("usertask1.do.myAssignee");
             task1.setOwner("usertask1.do.myOwner");
+            taskService.saveTask(task1);
+//            taskService.saveTask(task1);
 
 
             //可见: task
@@ -96,13 +122,7 @@ public class FlowableDemoService {
 
         //task2:
         //取得task : <userTask id="taskform2" name ="usertask2" flowable:formKey="default-approve-form">
-        Optional<Task> optTask2 = taskService.createTaskQuery()
-                .processInstanceId(processInstance.getId())
-                .taskDefinitionKey("taskform2")
-                .includeProcessVariables()
-                .includeTaskLocalVariables()
-                .listPage(0,1)
-                .stream().findFirst();
+        Optional<Task> optTask2 = _getTask(processInstance.getId(),"taskform2");
 
         if(optTask2.isPresent()){
             Task task = optTask2.get();
@@ -119,14 +139,10 @@ public class FlowableDemoService {
             System.out.println("7.taskinfo task2 after complete task2");
             flowableService.fetchTaskFromHistory(taskId);
 
-
         }else{
             System.out.println("not found task::" + "taskform2");
         }
-
-
     }
-
 
 
     //demo
@@ -171,4 +187,15 @@ public class FlowableDemoService {
         return flowableService.startProcess(Demo_FormProcessKey);
     }
 
+    //临时用，取得当前有效任务
+    private Optional<Task> _getTask(String processInstanceId, String taskKey){
+        Optional<Task> optTask = taskService.createTaskQuery()
+                .processInstanceId(processInstanceId)
+                .taskDefinitionKey(taskKey)
+                .includeProcessVariables()
+                .includeTaskLocalVariables()
+                .listPage(0,1)
+                .stream().findFirst();
+        return optTask;
+    }
 }
